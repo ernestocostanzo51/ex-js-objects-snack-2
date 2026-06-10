e9814b5212feb2069b6f6e4761f477f6783e9395
ernestocostanzo51 <ernestanzo51@gmail.com>
2026-06-10 18:35:01 +0200

    snack 4

---
+++ snack4/main.js
@@ -0,0 +1,21 @@
+const chef = {
+	name: "Chef Hyur",
+	age: 29,
+	makeBurger: (num = 1) => {
+		console.log(`Ecco ${num} hamburger per te!`);
+	},
+}
+
+const restaurant = {
+	name: "Hyur's Burgers",
+	address: {
+		street: 'Main Street',
+		number: 123,
+	},
+	openingDate: new Date(2025, 3, 11),
+	isOpen: false,
+};
+
+/* per lo chef meglio usare lo spread perchè all' interno ci sta una funzione
+
+   per restaurand meglio usare structureClone perchè all' interno è presente newDate e lo copia come tipo data()*/
